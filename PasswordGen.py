0e4c85f9b5704d62229745a993a26a2287334ac8
Salochin500 <110814545+Salochin500@users.noreply.github.com>
2022-09-22 15:38:20 +1000

    Create PasswordGen.py

---
+++ PasswordGen.py
@@ -0,0 +1,15 @@
+#generate password which is hrad to break for hackers
+
+import random
+
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIGKLMNOPQRSTUVWXYZ"
+number = "0123456789"
+symbols= "@#$%&*/\?"
+
+Use_for = lower_case + upper_case + number + symbols
+length_for_pass = 8
+
+password = "".join(random.sample(Use_for, lenght_for_pass))
+
+print("Your Generated Password")
